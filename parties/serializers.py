e584c62c4ff6a433230d26514ed378191f6aec11
cjworld <cjworld1208@gmail.com>
2014-03-25 02:36:40 +0800

    Serializers PartA

--- parties/serializers.py
+++ parties/serializers.py
@@ -2,33 +2,33 @@ from rest_framework import serializers
 from .models import User, Party, Photo, Message, ParticipationWillingness
 
 class UserSerializer(serializers.ModelSerializer):
-	parties = serializers.HyperlinkedIdentityField('parties', view_name='userparty-list', lookup_field='username')
+	parties = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='party-detail', lookup_field='author', required=False)
 	
 	class Meta:
 		model = User
 		fields = ('id', 'username', 'first_name', 'last_name', 'parties', )
 	
 class PhotoSerializer(serializers.ModelSerializer):
-    image = serializers.Field('image.url')
+	image = serializers.Field('image.url')
 
-    def get_validation_exclusions(self):
-        # Need to exclude `author` since we'll add that later based off the request
-        exclusions = super(PostSerializer, self).get_validation_exclusions()
-        return exclusions + ['author']
+	def get_validation_exclusions(self):
+		# Need to exclude `author` since we'll add that later based off the request
+		exclusions = super(PostSerializer, self).get_validation_exclusions()
+		return exclusions + ['author']
 	
-    class Meta:
-        model = Photo
+	class Meta:
+		model = Photo
 		
 class MessageSerializer(serializers.ModelSerializer):
 	author = UserSerializer(required=False)
 	
-    def get_validation_exclusions(self):
-        # Need to exclude `author` since we'll add that later based off the request
-        exclusions = super(PostSerializer, self).get_validation_exclusions()
-        return exclusions + ['author']
+	def get_validation_exclusions(self):
+		# Need to exclude `author` since we'll add that later based off the request
+		exclusions = super(PostSerializer, self).get_validation_exclusions()
+		return exclusions + ['author']
 	
-    class Meta:
-        model = Message
+	class Meta:
+		model = Message
 		fields = ('id', 'author', 'datetime', 'body', 'party', )
 		
 class PartySerializer(serializers.ModelSerializer):
@@ -37,24 +37,24 @@ class PartySerializer(serializers.ModelSerializer):
 	participationwillingnesses = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='participationwillingness-detail', required=False)
 	photos = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='photo-detail', required=False)
 
-    def get_validation_exclusions(self):
-        # Need to exclude `author` since we'll add that later based off the request
-        exclusions = super(PostSerializer, self).get_validation_exclusions()
-        return exclusions + ['author']
+	def get_validation_exclusions(self):
+		# Need to exclude `author` since we'll add that later based off the request
+		exclusions = super(PostSerializer, self).get_validation_exclusions()
+		return exclusions + ['author']
 	
-    class Meta:
-        model = Party
+	class Meta:
+		model = Party
 		fields = ('id', 'author', 'datetime', 'title', 'subscription', 'date', 'time', 'place', 'photos', 'messages', 'participationwillingnesses')
 
 	
 class ParticipationWillingnessSerializer(serializers.ModelSerializer):
 	author = UserSerializer(required=False)
 
-    def get_validation_exclusions(self):
-        # Need to exclude `author` since we'll add that later based off the request
-        exclusions = super(PostSerializer, self).get_validation_exclusions()
-        return exclusions + ['author']
+	def get_validation_exclusions(self):
+		# Need to exclude `author` since we'll add that later based off the request
+		exclusions = super(PostSerializer, self).get_validation_exclusions()
+		return exclusions + ['author']
 	
-    class Meta:
-        model = ParticipationWillingness
+	class Meta:
+		model = ParticipationWillingness
 		fields = ('id', 'author', 'participation', 'invitation', 'host', 'vegetarian', 'party', )
